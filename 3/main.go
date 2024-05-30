729ae552d68686a661f893f45bccb0fff5f9fb15
4lexrossi <devalexrossi@gmail.com>
2024-05-30 19:00:50 -0300

    file 3

--- 3/main.go
+++ 3/main.go
@@ -4,6 +4,14 @@ const a = "hello, world!"
 
 type ID int
 
+var (
+	b bool    = true
+	i int     = 10
+	s string  = "Alex"
+	f float64 = 1.2
+	d ID      = 1
+)
+
 func main() {
-	println(a)
+	println(d)
 }
