c778c8e4a7083b7e562e3ef7d13b6b3b7d9d6a17
4lexrossi <devalexrossi@gmail.com>
2024-06-14 14:29:11 -0300

    8-Testing-4-Testify

---
+++ 8-Testing/4-Testify/tax.go
@@ -0,0 +1,13 @@
+package tax
+
+import "errors"
+
+func CalculateTax(amount float64) (float64, error) {
+	if amount == 0 {
+		return 0.0, errors.New("amount tadada")
+	}
+	if amount >= 1000 {
+		return 10.0, nil
+	}
+	return 5.0, nil
+}
