284e8bedb0c6c8c197c051fc89b5c0ec8da81cf3
4lexrossi <devalexrossi@gmail.com>
2024-06-18 17:26:21 -0300

    feat: create user handler and user dto

---
+++ 9-APIs/internal/infra/webservers/handlers/user_handlers.go
@@ -0,0 +1,51 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/4lexRossi/pos-go/9-APIs/internal/dto"
+	"github.com/4lexRossi/pos-go/9-APIs/internal/entity"
+	"github.com/4lexRossi/pos-go/9-APIs/internal/infra/database"
+)
+
+type UserHandler struct {
+	UserDB database.UserInterface
+}
+
+func NewUserHandler(userDB database.UserInterface) *UserHandler {
+	return &UserHandler{UserDB: userDB}
+}
+
+func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
+	var user dto.CreateUserInput
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		msg := struct {
+			Message string `json:"message"`
+		}{
+			Message: err.Error(),
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(msg)
+		return
+	}
+	u, err := entity.NewUser(user.Name, user.Email, user.Password)
+	if err != nil {
+		msg := struct {
+			Message string `json:"message"`
+		}{
+			Message: err.Error(),
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(msg)
+		return
+	}
+	err = h.UserDB.Create(u)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
