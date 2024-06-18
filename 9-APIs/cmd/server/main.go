29840c04077bf8e5e07af573994fff4bc493c072
4lexrossi <devalexrossi@gmail.com>
2024-06-18 20:31:22 -0300

    feat: create token generation handler

--- 9-APIs/cmd/server/main.go
+++ 9-APIs/cmd/server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/4lexRossi/pos-go/9-APIs/configs"
 	"github.com/4lexRossi/pos-go/9-APIs/internal/entity"
 	"github.com/4lexRossi/pos-go/9-APIs/internal/infra/database"
 	"github.com/4lexRossi/pos-go/9-APIs/internal/infra/webservers/handlers"
@@ -13,10 +14,10 @@ import (
 )
 
 func main() {
-	// _, err := configs.LoadConfig(".")
-	// if err != nil {
-	// 	panic(err)
-	// }
+	configs, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
 	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	if err != nil {
 		panic(err)
@@ -26,17 +27,18 @@ func main() {
 	productHandler := handlers.NewProductHandler(productDB)
 
 	userDB := database.NewUser(db)
-	userHandler := handlers.NewUserHandler(userDB)
+	userHandler := handlers.NewUserHandler(userDB, configs.TokenAuth, configs.JwtExpiresIn)
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Post("/products", productHandler.CreateProduct)
 	r.Get("/products", productHandler.GetProducts)
-	r.Get("/products{id}", productHandler.GetProduct)
-	r.Put("/products{id}", productHandler.UpdateProduct)
+	r.Get("/products/{id}", productHandler.GetProduct)
+	r.Put("/products/{id}", productHandler.UpdateProduct)
 	r.Delete("/products{id}", productHandler.DeleteProduct)
 
 	r.Post("/users", userHandler.CreateUser)
+	r.Post("/users/generate_token", userHandler.GetJWT)
 
 	http.ListenAndServe(":8000", r)
 }
--- 9-APIs/internal/infra/webservers/handlers/user_handlers.go
+++ 9-APIs/internal/infra/webservers/handlers/user_handlers.go
@@ -3,18 +3,58 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/4lexRossi/pos-go/9-APIs/internal/dto"
 	"github.com/4lexRossi/pos-go/9-APIs/internal/entity"
 	"github.com/4lexRossi/pos-go/9-APIs/internal/infra/database"
+	"github.com/go-chi/jwtauth"
 )
 
 type UserHandler struct {
-	UserDB database.UserInterface
+	UserDB       database.UserInterface
+	Jwt          *jwtauth.JWTAuth
+	JwtExpiresIn int
 }
 
-func NewUserHandler(userDB database.UserInterface) *UserHandler {
-	return &UserHandler{UserDB: userDB}
+func NewUserHandler(userDB database.UserInterface, jwt *jwtauth.JWTAuth, jwtEpiresIn int) *UserHandler {
+	return &UserHandler{
+		UserDB:       userDB,
+		Jwt:          jwt,
+		JwtExpiresIn: jwtEpiresIn,
+	}
+}
+
+func (h *UserHandler) GetJWT(w http.ResponseWriter, r *http.Request) {
+	var user dto.GetJWTInput
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	u, err := h.UserDB.FindByEmail(user.Email)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	if !u.ValidatePassword(user.Password) {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	_, tokenString, _ := h.Jwt.Encode(map[string]interface{}{
+		"sub": u.ID.String(),
+		"exp": time.Now().Add(time.Second * time.Duration(h.JwtExpiresIn)).Unix(),
+	})
+	accessToken := struct {
+		AccessToken string `json:"access_token`
+	}{
+		AccessToken: tokenString,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(accessToken)
 }
 
 func (h *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
