cde3ee0f882c54bfff209bc14bfbc2520855d1d9
4lexrossi <devalexrossi@gmail.com>
2024-06-18 17:01:04 -0300

    feat: add create and update product handler

--- 9-APIs/cmd/server/main.go
+++ 9-APIs/cmd/server/main.go
@@ -3,19 +3,20 @@ package main
 import (
 	"net/http"
 
-	"github.com/4lexRossi/pos-go/9-APIs/configs"
 	"github.com/4lexRossi/pos-go/9-APIs/internal/entity"
 	"github.com/4lexRossi/pos-go/9-APIs/internal/infra/database"
 	"github.com/4lexRossi/pos-go/9-APIs/internal/infra/webservers/handlers"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 func main() {
-	_, err := configs.LoadConfig(".")
-	if err != nil {
-		panic(err)
-	}
+	// _, err := configs.LoadConfig(".")
+	// if err != nil {
+	// 	panic(err)
+	// }
 	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	if err != nil {
 		panic(err)
@@ -24,7 +25,11 @@ func main() {
 	productDB := database.NewProduct(db)
 	productHandler := handlers.NewProductHandler(productDB)
 
-	http.HandleFunc("/products", productHandler.CreateProduct)
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Post("/products", productHandler.CreateProduct)
+	r.Get("/products{id}", productHandler.GetProduct)
+	r.Put("/products{id}", productHandler.UpdateProduct)
 
-	http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(":8000", r)
 }
--- 9-APIs/internal/infra/webservers/handlers/product_handlers.go
+++ 9-APIs/internal/infra/webservers/handlers/product_handlers.go
@@ -7,6 +7,9 @@ import (
 	"github.com/4lexRossi/pos-go/9-APIs/internal/dto"
 	"github.com/4lexRossi/pos-go/9-APIs/internal/entity"
 	"github.com/4lexRossi/pos-go/9-APIs/internal/infra/database"
+
+	entityPkg "github.com/4lexRossi/pos-go/9-APIs/pkg/entity"
+	"github.com/go-chi/chi"
 )
 
 type ProductHandler struct {
@@ -50,4 +53,52 @@ func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.WriteHeader(http.StatusCreated)
+}
+
+func (h *ProductHandler) GetProduct(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	product, err := h.ProductDB.FindByID(id)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(product)
+}
+
+func (h *ProductHandler) UpdateProduct(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	var product entity.Product
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	product.ID, err = entityPkg.ParseID(id)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+	_, err = h.ProductDB.FindByID(id)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	err = h.ProductDB.Update(&product)
+	if err != nil {
+		w.WriteHeader((http.StatusInternalServerError))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
