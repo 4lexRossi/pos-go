350a727b1438be210c11946ddc4f5e0d92326940
4lexrossi <devalexrossi@gmail.com>
2024-06-25 16:16:43 -0300

    11-Events-Dispatcher-Dispatch-Async

--- 11-Events/pkg/events/event_dispatcher.go
+++ 11-Events/pkg/events/event_dispatcher.go
@@ -1,6 +1,9 @@
 package events
 
-import "errors"
+import (
+	"errors"
+	"sync"
+)
 
 var ErrHandlerAlreadyExist = errors.New("handler already registerede")
 
@@ -16,9 +19,12 @@ func NewEventDispatcher() *EventDispatcher {
 
 func (ed *EventDispatcher) Dispatch(event EventInterface) error {
 	if handlers, ok := ed.handlers[event.GetName()]; ok {
+		wg := &sync.WaitGroup{}
 		for _, handler := range handlers {
-			handler.Handle(event)
+			wg.Add(1)
+			handler.Handle(event, wg)
 		}
+		wg.Wait()
 	}
 	return nil
 }
