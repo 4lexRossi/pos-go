8255293425fe3bfcf417723b86fc8394ad725747
4lexrossi <devalexrossi@gmail.com>
2024-07-04 14:02:25 -0300

    13-GraphQl-CreateCategory-And-Persist-Data

--- 13-GraphQL/cmd/server/server.go
+++ 13-GraphQL/cmd/server/server.go
@@ -1,24 +1,37 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/4lexRossi/pos-go/13-GraphQL/graph"
+	"github.com/4lexRossi/pos-go/13-GraphQL/internal/database"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 const defaultPort = "8080"
 
 func main() {
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatalf("failed to open DB: %v", err)
+	}
+	defer db.Close()
+
+	categoryDB := database.NewCategory(db)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+		CategoryDB: categoryDB,
+	}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
